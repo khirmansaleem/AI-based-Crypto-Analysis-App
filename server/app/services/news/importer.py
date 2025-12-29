a4639a57c11c811fa1757ad873e5f30a5ac8b30b
Khirman Saleem <khirmansaleem137@gmail.com>
2025-12-29 22:17:01 +0500

    add changes to server

--- server/app/services/news/importer.py
+++ server/app/services/news/importer.py
@@ -1,47 +1,47 @@
 import os
 import shutil
-from requests import Session
+from fastapi import logger
 from app.services.news.article_service import insert_article
 from app.services.news.load_scraped_articles import load_all_scraped_articles
 from app.services.news.paths import FAILED_DIR
+from app.database.database import SessionLocal
 
 
-def import_scraped_articles_core(db: Session):
+def import_scraped_articles_core():
     articles = load_all_scraped_articles()
     response = []
 
-    for art in articles:
-        result = insert_article(
-            db=db,
-            title=art["title"],
-            url=art["url"],
-            content=art["content"],
-            category=art["category"],
-            published_at=art["published_at"],
-        )
-
-        response.append(
-            {
-                "file": art["filename"],
-                "status": result["status"],
-                "id": result.get("id"),
-                "error": result.get("error"),
-            }
-        )
-
-        # ✅ SUCCESS → DELETE FILE
-        if result["status"] in ("inserted", "exists"):
-            try:
-                os.remove(art["filepath"])
-            except Exception as e:
-                # optional: log this
-                print(f"Failed to delete {art['filepath']}: {e}")
-
-        # ❌ FAILURE → MOVE TO FAILED
-        else:
-            shutil.move(
-                art["filepath"],
-                os.path.join(FAILED_DIR, art["filename"]),
+    with SessionLocal() as db:
+        for art in articles:
+            result = insert_article(
+                db=db,
+                title=art["title"],
+                url=art["url"],
+                content=art["content"],
+                category=art["category"],
+                published_at=art["published_at"],
             )
+
+            response.append(
+                {
+                    "file": art["filename"],
+                    "status": result["status"],
+                    "id": result.get("id"),
+                    "error": result.get("error"),
+                }
+            )
+
+            if result["status"] in ("inserted", "exists"):
+                try:
+                    os.remove(art["filepath"])
+                except Exception as e:
+                    logger.warning(f"Failed to delete {art['filepath']}: {e}")
+            else:
+                shutil.move(
+                    art["filepath"],
+                    os.path.join(FAILED_DIR, art["filename"]),
+                )
+
+        db.commit()
 
     return response
