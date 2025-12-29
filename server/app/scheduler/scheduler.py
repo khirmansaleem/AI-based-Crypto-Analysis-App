e1462c7bdc164fd235e6a4f68b205a2deadee891
Khirman Saleem <khirmansaleem137@gmail.com>
2025-12-30 03:15:05 +0500

    add changes to server

--- server/app/scheduler/scheduler.py
+++ server/app/scheduler/scheduler.py
@@ -32,7 +32,7 @@ def start_scheduler():
     # =====================================================
     # ✅ DAILY CRON — 01:20 AM PKT (20:20 UTC)
     # =====================================================
-    trigger = CronTrigger(hour=21, minute=50)
+    trigger = CronTrigger(hour=22, minute=20)
 
     scheduler.add_job(
         _run_daily_news_pipeline,
